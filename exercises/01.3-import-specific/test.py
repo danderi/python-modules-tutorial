7ef06e1174eca8ff2115a14f92aa9db3dd503be4
Rosinni Rodríguez <43686922+rosinni@users.noreply.github.com>
2025-01-20 14:04:55 +0100

    Update test.py

--- exercises/01.3-import-specific/test.py
+++ exercises/01.3-import-specific/test.py
@@ -22,30 +22,3 @@ def test_print_output():
             output = stdout.getvalue().strip()
     assert output == 'apple', f"The output is not as expected. Got: {output}"
 
-
-
-
-
-# import unittest
-# from unittest.mock import patch
-# from io import StringIO
-# import sys
-
-# class TestRandomChoice(unittest.TestCase):
-#     @patch('random.choice', return_value='apple')
-#     def test_random_choice(self, mock_choice):
-#         # Capture the output
-#         captured_output = StringIO()
-#         sys.stdout = captured_output
-        
-#         # Import the module to test
-#         import app
-        
-#         # Reset redirect.
-#         sys.stdout = sys.__stdout__
-        
-#         # Check the output
-#         self.assertEqual(captured_output.getvalue().strip(), 'apple')
-
-# if __name__ == '__main__':
-#     unittest.main()
